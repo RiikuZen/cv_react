0c6e797b58909e4d2aa5339d645d0eae258da542
Nico-C <nicolas.cauet@yahoo.fr>
2021-11-02 17:25:44 +0100

    Fixed path (URI)

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -15,25 +15,25 @@ const Navigation = () => {
             <div className="navigation"> 
                 <ul>
                     <li>
-                        <NavLink exact to="/" activeClassName="navActive">
+                        <NavLink exact to="/cv_react" activeClassName="navActive">
                             <i className="fas fa-home"></i>
                             <span>Accueil</span>
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink exact to="/competences" activeClassName="navActive">
+                        <NavLink exact to="/cv_react/competences" activeClassName="navActive">
                             <i className="fas fa-mountain"></i>
                             <span>Compétences</span>
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink exact to="/portfolio" activeClassName="navActive">
+                        <NavLink exact to="/cv_react/portfolio" activeClassName="navActive">
                             <i className="fas fa-image"></i> 
                             <span>Portfolio</span>
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink exact to="/contact" activeClassName="navActive">
+                        <NavLink exact to="/cv_react/contact" activeClassName="navActive">
                             <i className="fas fa-address-book"></i>
                             <span>Contact</span>
                         </NavLink>
